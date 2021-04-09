29f638914c930e50a2a74c7dd62e439dd3019c4d
unknown <Red Angeles>
2021-04-09 16:04:30 +0800

    updated our menu-items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router';
+
 
 import HomePage from './pages/homepage/homepage.component';
 
+
+const Hatspage = () => (
+  <div>
+    <h1> HATS PAGE </h1>
+  </div>
+);
+
+const Jacketspage = () => (
+  <div>
+    <h1> JACKET PAGE </h1>
+  </div>
+)
+
 function App() {
   return(
-     <div> <HomePage /> </div>
+     <div> 
+       <Switch>
+         <Route exact path='/' component ={HomePage} />
+         <Route path='/hats' component={Hatspage} />
+         <Route path='/jackets' component={Jacketspage} />
+       </Switch>
+      </div>
   );
 } 
+
+
+
 
 export default App;
